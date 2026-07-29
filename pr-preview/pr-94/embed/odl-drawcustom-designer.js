3357a5c6cf183dc725932e80736849bd5f417c7c
schlomo <schlomo@users.noreply.github.com>
2026-07-29 18:16:49 +0000

    Deploy preview for PR 94 🛫

--- pr-preview/pr-94/embed/odl-drawcustom-designer.js
+++ pr-preview/pr-94/embed/odl-drawcustom-designer.js
@@ -8018,7 +8018,7 @@ function ne(e) {
 }
 //#endregion
 //#region src/core/buildInfo.ts
-var re = /* @__PURE__ */ new Set(["dev", "test"]), ie = "feat/issue-93-auto-release", ae = "cc176e3", k = "12ba12d", A = "0.0.0-dev";
+var re = /* @__PURE__ */ new Set(["dev", "test"]), ie = "feat/issue-93-auto-release", ae = "503163c", k = "21457b6", A = "0.0.0-dev";
 function j(e, t = 12) {
 	if (re.has(e) || e.length <= t) return e;
 	let n = e.includes("/") ? e.slice(e.lastIndexOf("/") + 1) : e;
