c1c196e88ed6fd2b2859d4ad7239f6bbb3def33f
schlomo <schlomo@users.noreply.github.com>
2026-07-30 09:16:14 +0000

    Deploy preview for PR 95 🛫

--- pr-preview/pr-95/embed/odl-drawcustom-designer.js
+++ pr-preview/pr-95/embed/odl-drawcustom-designer.js
@@ -8018,7 +8018,7 @@ function ne(e) {
 }
 //#endregion
 //#region src/core/buildInfo.ts
-var re = /* @__PURE__ */ new Set(["dev", "test"]), ie = "feat/issue-72-host-adapter-seam", ae = "3de1139", k = "e73de14", A = "0.0.0-dev";
+var re = /* @__PURE__ */ new Set(["dev", "test"]), ie = "feat/issue-72-host-adapter-seam", ae = "b9524df", k = "b175b74", A = "0.0.0-dev";
 function j(e, t = 12) {
 	if (re.has(e) || e.length <= t) return e;
 	let n = e.includes("/") ? e.slice(e.lastIndexOf("/") + 1) : e;
