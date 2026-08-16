2f0228059d36a3a201c355e717d9d7ae8995a0e7
schlomo <schlomo@users.noreply.github.com>
2026-08-16 13:39:09 +0000

    Deploy preview for PR 129 🛫

--- pr-preview/pr-129/embed/odl-drawcustom-designer.js
+++ pr-preview/pr-129/embed/odl-drawcustom-designer.js
@@ -8018,7 +8018,7 @@ function ne(e) {
 }
 //#endregion
 //#region src/core/buildInfo.ts
-var re = /* @__PURE__ */ new Set(["dev", "test"]), ie = "feat/issue-108-actions-seam", ae = "c009b90", oe = "fcfd1a8", k = "0.0.0-dev";
+var re = /* @__PURE__ */ new Set(["dev", "test"]), ie = "feat/issue-108-actions-seam", ae = "086016a", oe = "964dff9", k = "0.0.0-dev";
 function A(e, t = 12) {
 	if (re.has(e) || e.length <= t) return e;
 	let n = e.includes("/") ? e.slice(e.lastIndexOf("/") + 1) : e;
