7f18ef28ef8c2edfb8b244e49531276bef618e0d
schlomo <schlomo@schapiro.org>
2026-08-16 13:29:26 +0000

    Deploying to gh-pages from @ schlomo/odl-drawcustom-designer@986819b3cdbc2901e1e2988abdac2165f075f7f6 🚀

--- embed/odl-drawcustom-designer.js
+++ embed/odl-drawcustom-designer.js
@@ -8018,7 +8018,7 @@ function ne(e) {
 }
 //#endregion
 //#region src/core/buildInfo.ts
-var re = /* @__PURE__ */ new Set(["dev", "test"]), ie = "main", k = "02a2307", A = "02a2307", j = "0.0.0-dev";
+var re = /* @__PURE__ */ new Set(["dev", "test"]), ie = "main", k = "986819b", A = "986819b", j = "0.0.0-dev";
 function M(e, t = 12) {
 	if (re.has(e) || e.length <= t) return e;
 	let n = e.includes("/") ? e.slice(e.lastIndexOf("/") + 1) : e;
